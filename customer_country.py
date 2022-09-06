f1a73bf94a5507d5828ca252c0e18cb1e6af663c
Thomas <thomas_zelazny1@baylor.edu>
2022-09-06 12:41:19 -0500

    customer_country changes

--- customer_country.py
+++ customer_country.py
@@ -5,7 +5,7 @@ outfile=open("customer_country.csv","w")
 
 csvfile=csv.reader(infile, delimiter=',')
 
-outfile.write("FullName, Country")
+
 
 for customer in csvfile:
     outfile.write(customer[1]+" "+customer[2]+","+" "+customer[4])
